3ce11813ab6bdf6744e0065df0c3c8f1c6c79530
omu-aaa23057 <so23319h@st.omu.ac.jp>
2025-12-03 02:07:02 +0000

    Pending changes exported from your codespace

--- week03/main.go
+++ week03/main.go
@@ -1,13 +1,30 @@
 package main
 
-import "fmt"
+import (
+    "fmt"
+    "math/rand"
+    "net/http"
+    "time"
+)
 
 func main() {
-	// Week 03: ここに課題のコードを記述してください
-	// 詳細な課題内容はLMSで確認してください
-	
-	fmt.Println("Week 03 課題")
-	
-	// 以下に実装してください
-	
+    // 乱数のタネを現在時刻で初期化
+    rand.Seed(time.Now().UnixNano())
+
+    // /webfortune にアクセスが来たら fortuneHandler を呼ぶ
+    http.HandleFunc("/webfortune", fortuneHandler)
+
+    // ポート8080でWebサーバを起動
+    http.ListenAndServe(":8080", nil)
+}
+
+// おみくじ用ハンドラ
+func fortuneHandler(w http.ResponseWriter, r *http.Request) {
+    fortunes := []string{"大吉", "中吉", "吉", "凶"}
+
+    // 0〜len(fortunes)-1 の乱数
+    n := rand.Intn(len(fortunes))
+
+    // ブラウザに表示する文言
+    fmt.Fprintf(w, "今の運勢は%sです。\n", fortunes[n])
 }
--- week04/main.go
+++ week04/main.go
@@ -1,13 +1,29 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+	"time"
+)
 
 func main() {
-	// Week 04: ここに課題のコードを記述してください
-	// 詳細な課題内容はLMSで確認してください
-	
-	fmt.Println("Week 04 課題")
-	
-	// 以下に実装してください
-	
+	// /info にアクセスが来たときに infoHandler を呼び出す
+	http.HandleFunc("/info", infoHandler)
+
+	// ポート8080でサーバを起動
+	fmt.Println("Server is running on http://localhost:8080/info")
+	http.ListenAndServe(":8080", nil)
+}
+
+// /info にアクセスがあったときに実行される関数
+func infoHandler(w http.ResponseWriter, r *http.Request) {
+	// JST(日本時間)を取得
+	jst, _ := time.LoadLocation("Asia/Tokyo")
+	now := time.Now().In(jst).Format("2006年01月02日 15:04:05")
+
+	// ブラウザ情報(User-Agent)を取得
+	ua := r.Header.Get("User-Agent")
+
+	// クライアントにメッセージを返す
+	fmt.Fprintf(w, "今の時刻は%sで，利用しているブラウザは「%s」，ですね。", now, ua)
 }
